2f0640a1acb25b7c6fb1d5e33e43782e06fcd538
Ruslan Honchar <ruslan.honchar@globallogic.com>
2021-02-10 23:27:38 +0200

    Remove outline

--- pages/index.vue
+++ pages/index.vue
@@ -13,7 +13,6 @@
                 item-value="link"
                 label="Predefined video options"
                 return-object
-                outlined
               />
             </v-col>
             <v-col cols="12" md="6">
